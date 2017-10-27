34f50d0da02cf9000a52c75f89f32447a8e11d62
Zoythrus <32144955+Zoythrus@users.noreply.github.com>
2017-10-27 13:00:28 -0700

    Shotgun prototype

--- entities/weapons/war_shotgun.lua
+++ entities/weapons/war_shotgun.lua
@@ -20,6 +20,8 @@ SWEP.AdminOnly			= false
 
 SWEP.Category = "Weapons"
 
+SWEP.MaxAmmo = 30
+
 SWEP.Primary.Damage = 8
 SWEP.Primary.TakeAmmo = 1
 SWEP.Primary.Clip1 = 8
@@ -36,7 +38,7 @@ SWEP.Primary.Force = 0
 SWEP.HeadshotMultiplier    = 1.25
 
 SWEP.UseHands = false
-SWEP.Primary.Sound = "weapons/shotgun/fire1.wav"
+SWEP.Primary.Sound = "weapons/shotgun/single.wav"
 SWEP.ReloadSound = "weapons/shotgun/shotgun_reload.wav"
 
 
@@ -87,123 +89,36 @@ function SWEP:SetupDataTables()
 end
 
 function SWEP:Reload()
+	if self:Clip1() < self.Primary.ClipSize then
+		local clips = self:Clip1()
+		print(clips)
+		clipslize = self.Primary.Clipsize
+		self.Weapon:SendWeaponAnim( ACT_SHOTGUN_RELOAD_START)
 
-   --if self:GetNWBool( "reloading", false ) then return end
-   if self:GetReloading() then return end
+		for i= clips,8,1 do
+			local reloading = true
+			if reloading = true then
+			
+				timer.Simple(1, function()
+				local cliptemp = i
+				self:EmitSound(Sound(self.ReloadSound))
+				self.Weapon:SendWeaponAnim( ACT_RELOAD_SHOTGUN)
+				print(cliptemp.." is how much is in the clip")
+				self:SetClip1(cliptemp)
+				print(self:Clip1())
+				reloading = false
+				end)
 
-   if self:Clip1() < self.Primary.ClipSize and self:GetOwner():GetAmmoCount( self.Primary.Ammo ) > 0 then
-
-      if self:StartReload() then
-         return
-      end
-   end
-
-end
-
-function SWEP:StartReload()
-   --if self:GetNWBool( "reloading", false ) then
-   if self:GetReloading() then
-      return false
-   end
-
-
-   self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
-
-   local ply = self:GetOwner()
-
-   if not ply or ply:GetAmmoCount(self.Primary.Ammo) <= 0 then
-      return false
-   end
-
-   local wep = self
-
-   if wep:Clip1() >= self.Primary.ClipSize then
-      return false
-   end
-
-   wep:SendWeaponAnim(ACT_SHOTGUN_RELOAD_START)
-
-   self:SetReloadTimer(CurTime() + wep:SequenceDuration())
-
-   --wep:SetNWBool("reloading", true)
-   self:SetReloading(true)
-
-   return true
-end
-
-function SWEP:PerformReload()
-   local ply = self:GetOwner()
-
-   -- prevent normal shooting in between reloads
-   self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
-
-   if not ply or ply:GetAmmoCount(self.Primary.Ammo) <= 0 then return end
-
-   if self:Clip1() >= self.Primary.ClipSize then return end
-
-   self:GetOwner():RemoveAmmo( 1, self.Primary.Ammo, false )
-   self:SetClip1( self:Clip1() + 1 )
-
-   self:SendWeaponAnim(ACT_VM_RELOAD)
-
-   self:SetReloadTimer(CurTime() + self:SequenceDuration())
-end
-
-function SWEP:FinishReload()
-   self:SetReloading(false)
-   self:SendWeaponAnim(ACT_SHOTGUN_RELOAD_FINISH)
-
-   self:SetReloadTimer(CurTime() + self:SequenceDuration())
-end
-
-function SWEP:CanPrimaryAttack()
-   if self:Clip1() <= 0 then
-      self:EmitSound( "Weapon_Shotgun.Empty" )
-      self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
-      return false
-   end
-   return true
+			end
+		end
+			self.Weapon:SendWeaponAnim( ACT_SHOTGUN_RELOAD_FINISH)
+		
+	end
 end
 
 function SWEP:Think()
-   --weapon_base.Think(self)
-   if self:GetReloading() then
-      if self:GetOwner():KeyDown(IN_ATTACK) then
-         self:FinishReload()
-         return
-      end
-
-      if self:GetReloadTimer() <= CurTime() then
-
-         if self:GetOwner():GetAmmoCount(self.Primary.Ammo) <= 0 then
-            self:FinishReload()
-         elseif self:Clip1() < self.Primary.ClipSize then
-            self:PerformReload()
-         else
-            self:FinishReload()
-         end
-         return
-      end
-   end
 end
 
-function SWEP:Deploy()
-   self:SetReloading(false)
-   self:SetReloadTimer(0)
-   return weapon_base.Deploy(self)
-end
-
--- The shotgun's headshot damage multiplier is based on distance. The closer it
--- is, the more damage it does. This reinforces the shotgun's role as short
--- range weapon by reducing effectiveness at mid-range, where one could score
--- lucky headshots relatively easily due to the spread.
-function SWEP:GetHeadshotMultiplier(victim, dmginfo)
-   local att = dmginfo:GetAttacker()
-   if not IsValid(att) then return 3 end
-
-   local dist = victim:GetPos():Distance(att:GetPos())
-   local d = math.max(0, dist - 140)
-
-   -- decay from 3.1 to 1 slowly as distance increases
-   return 1 + math.max(0, (2.1 - 0.002 * (d ^ 1.25)))
+function SWEP:GetMaxAmmo()
+	return self.MaxAmmo
 end
