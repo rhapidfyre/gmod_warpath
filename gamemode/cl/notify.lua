818b47a37bf0d526eca5736e4ab299b4faf76968
rhapidfyre <maharris84@hotmail.com>
2017-11-06 09:44:53 -0800

    Implementing endgame notification window (stats, etc)

--- gamemode/cl/notify.lua
+++ gamemode/cl/notify.lua
@@ -1,4 +1,33 @@
 
+local lastShow = CurTime()
+local endgame = nil
+
+local function DisplayVictory(victor)
+	
+	if lastShow > CurTime() then
+		if endgame == nil then
+			local endgame = vgui.Create("DFrame")
+			endgame:SetSize(ScrW()*0.75, ScrH()*0.4)
+			endgame:Center()
+			endgame:ShowCloseButton(false)
+			
+			
+			
+			endgame:MakePopup()
+		end
+		if endgame ~= nil then
+			if !endgame:IsVisible() then endgame:SetVisible(true) end
+		end
+	else
+		if endgame ~= nil then
+			endgame:Close()
+			endgame:Remove()
+			endgame = nil
+		end
+	end	
+	
+end
+
 net.Receive("SV_Capture", function(len, ply)
     
     local point  = net.ReadInt(8)
@@ -33,5 +62,7 @@ net.Receive("SV_Victory", function(len, ply)
     else
         surface.PlaySound("vox/all_captured.wav")
     end
-
+	lastShow = CurTime() + TIME_END
+	DisplayVictory(victor)
+	
 end)
